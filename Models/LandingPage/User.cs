cbe90f615f331d6293f9d46dc61cd53d1f53b5d1
shvz99 <shakhrizoda29@gmail.com>
2024-10-31 21:09:09 +0000

    landing

---
+++ Models/LandingPage/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GlucoSeeTracker.Models.LandingPage
+{
+    public class User
+    {
+        public int UserId { get; set; } //Primary key
+
+        [Required(ErrorMessage = "Please enter a username")]
+        public string? Username { get; set; }
+
+        
+    }
+
+    public class Password
+    { 
+        public string? PasswordId { get; set; }
+    }
+}
